d85fa8c8b6bbb8d8a6c55fe08f5b825eb9bf0805
magb7 <margaux.bedu@gmail.com>
2020-04-19 17:03:49 +0200

    create component .js add style sheet

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="snake-icon"></div>
+      <h1>Snake Game</h1>
+    </header>
+  );
+};
+
+export default Header;
