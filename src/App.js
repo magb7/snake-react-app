562e3312fe1fcca65755da189953d57088c7e571
magb7 <margaux.bedu@gmail.com>
2020-04-26 16:16:22 +0200

    add footer section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Game from "./components/Game";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="back-img">
-        <Header />
-        <Game />
+      <div>
+        <div className="back-img">
+          <Header />
+          <Game />
+        </div>
+        <Footer />
       </div>
     );
   }
