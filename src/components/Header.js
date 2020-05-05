7296a4305a431846397a968d08410676d6aca2ed
magb7 <margaux.bedu@gmail.com>
2020-05-05 17:15:21 +0200

    add link through home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-      <div className="snake-icon"></div>
+      <Link to="/">
+        <div className="snake-icon"></div>
+      </Link>
       <h1>Snake Game</h1>
     </header>
   );
