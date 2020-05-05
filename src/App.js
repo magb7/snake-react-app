4f2867071ac251848fabdceae83129eeba586434
magb7 <margaux.bedu@gmail.com>
2020-05-05 17:14:43 +0200

    add home component and browser router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Components
 import Footer from "./components/Footer";
 import Game from "./components/Game";
 import Header from "./components/Header";
-import "./App.css";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
@@ -10,7 +14,10 @@ class App extends Component {
       <div>
         <div className="back-img">
           <Header />
-          <Game />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Game" component={Game} />
+          </Switch>
         </div>
         <Footer />
       </div>
