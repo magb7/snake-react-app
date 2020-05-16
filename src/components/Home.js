82b318b8c1360690eef0e3bbaae99d13093f5350
magb7 <margaux.bedu@gmail.com>
2020-05-16 15:56:14 +0200

    add text for reponsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,16 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <h3>Use the keyboard arrows to play</h3>
-      <img src="/images/keyboardArrows.png" alt="keyboard arrows" />
-      <Link to="/Game">
-        <button className="start">Press to start</button>
-      </Link>
+      <div className="desktop">
+        <h3>Use the keyboard arrows to play</h3>
+        <img src="/images/keyboardArrows.png" alt="keyboard arrows" />
+        <Link to="/Game">
+          <button className="start">Press to start</button>
+        </Link>
+      </div>
+      <div className="responsive">
+        <h3 className="resp-text">The game is available on computer only</h3>
+      </div>
     </div>
   );
 };
