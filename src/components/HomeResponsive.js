8d78cc36e2b0d9b2583d15779ec44f98e5cf3773
magb7 <margaux.bedu@gmail.com>
2020-05-23 19:43:35 +0200

    create HomeResponsive component

---
+++ src/components/HomeResponsive.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  align-items: center;
+  display: none;
+  text-align: center;
+  margin: 30px;
+  width: 70vw;
+  height: 60vh;
+
+  @media only screen and (max-width: 1024px) {
+    display: flex;
+  }
+`;
