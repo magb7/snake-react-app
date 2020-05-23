4cada210f4a47cc75d8a8f40a52a6b857d17f9df
magb7 <margaux.bedu@gmail.com>
2020-05-23 16:28:00 +0200

    create ToggleWrapper component

---
+++ src/components/ToggleWrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 83px;
+  height: 30px;
+  background: rgba(9, 169, 3, 0.7);
+  border-radius: 50px;
+  padding: 5px 9px;
+  cursor: pointer;
+`;
