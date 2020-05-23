24dae56d9a090bfde5875ef331667506e8142dd2
magb7 <margaux.bedu@gmail.com>
2020-05-23 19:44:30 +0200

    chnage line-height

--- src/components/H3.js
+++ src/components/H3.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default styled.h3`
   color: #09a903;
   text-shadow: 3px 2px black;
-  line-height: 2rem;
+  line-height: 3rem;
   letter-spacing: 0.1rem;
 
   @media only screen and (max-width: 600px) {
