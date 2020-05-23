1b9ffa53f966e91c89c46eb8d9370eb64aae68af
magb7 <margaux.bedu@gmail.com>
2020-05-23 19:37:04 +0200

    create HmaeDesktop component

---
+++ src/components/HomeDesktop.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export default styled.div`
+  justify-content: space-around;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px;
+  width: 60vw;
+  height: 60vh;
+  border: 1px solid blue;
+  text-align: center;
+
+  img {
+    width: 180px;
+    height: 120px;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
