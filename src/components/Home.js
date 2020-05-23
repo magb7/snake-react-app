6e253bc752f5b90a76b686e9760ab174eabb2533
magb7 <margaux.bedu@gmail.com>
2020-05-23 15:28:38 +0200

    add h3 styled component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+// Components
+import H3 from "./H3";
+
 const Home = () => {
   return (
     <div className="home">
       <div className="desktop">
-        <h3>Use the keyboard arrows to play</h3>
+        <H3>Use the keyboard arrows to play</H3>
         <img src="/images/keyboardArrows.png" alt="keyboard arrows" />
         <Link to="/Game">
           <button className="start">Press to start</button>
         </Link>
       </div>
       <div className="responsive">
-        <h3 className="resp-text">The game is available on computer only</h3>
+        <H3 className="resp-text">The game is available on computer only</H3>
       </div>
     </div>
   );
