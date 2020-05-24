d096db569d9da02b63b3262c297c65be4c0a9560
magb7 <margaux.bedu@gmail.com>
2020-05-24 14:29:19 +0200

    create the GameArea component

---
+++ src/components/GameArea.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.div`
+  align-self: center;
+  position: relative;
+  margin: 30px;
+  width: 600px;
+  height: 450px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border: 3px solid #09a903;
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
