f60e42e9dc9521b3dcc6f219b0d3787a8ae32cb3
magb7 <margaux.bedu@gmail.com>
2020-05-24 14:22:26 +0200

    add conditional display for score

--- src/components/H3.js
+++ src/components/H3.js
@@ -5,6 +5,9 @@ export default styled.h3`
   text-shadow: 3px 2px black;
   line-height: 3rem;
   letter-spacing: 0.1rem;
+  position: ${(props) => (props.score ? "absolute" : "")};
+  top: ${(props) => (props.score ? "-45px" : "")};
+  left: ${(props) => (props.score ? "220px" : "")};
 
   @media only screen and (max-width: 600px) {
     font-size: 1.5rem;
