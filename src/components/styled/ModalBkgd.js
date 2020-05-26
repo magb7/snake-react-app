289ffa42859a72251e39243fedfcf167ad15fdbb
magb7 <margaux.bedu@gmail.com>
2020-05-26 18:28:18 +0200

    change width and height

--- src/components/styled/ModalBkgd.js
+++ src/components/styled/ModalBkgd.js
@@ -5,8 +5,8 @@ export default styled.div`
   justify-content: center;
   align-items: center;
   position: absolute;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   top: 0;
   left: 0;
   background-color: rgba(9, 169, 3, 0.4);
