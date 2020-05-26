fe936cbe3f745e691ef14a62c5066df28ab9b601
magb7 <margaux.bedu@gmail.com>
2020-05-26 15:56:06 +0200

    create modal background component

---
+++ src/components/styled/ModalBkgd.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(9, 169, 3, 0.4);
+  z-index: 2;
+`;
