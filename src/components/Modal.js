5fa82a5e00f0adc905e65c99b683dfc7b4c957f1
magb7 <margaux.bedu@gmail.com>
2020-05-26 15:56:33 +0200

    create the Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Components
+import ModalBkgd from "./styled/ModalBkgd";
+
+const Modal = () => {
+  return <ModalBkgd>HELLO WORLD !</ModalBkgd>;
+};
+
+export default Modal;
