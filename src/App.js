9e16e0c745ed3a5b18b4cc2e9384b6d43777c79a
magb7 <margaux.bedu@gmail.com>
2020-06-06 17:45:30 +0200

    import store and add redux and darkTheme providers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 // Components
+import Container from "./components/styled/Container";
 import Footer from "./components/Footer";
 import Game from "./components/Game";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Main from "./components/styled/Main";
+import DarkThemeProvider from "./components/DarkThemeProvider";
+// Store
+import store from "./store";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Main>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/Game" component={Game} />
-        </Switch>
-      </Main>
-      <Footer />
-    </div>
+    <ReduxProvider store={store}>
+      <DarkThemeProvider>
+        <Container>
+          <Header />
+          <Main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/Game" component={Game} />
+            </Switch>
+          </Main>
+          <Footer />
+        </Container>
+      </DarkThemeProvider>
+    </ReduxProvider>
   );
 };
 
