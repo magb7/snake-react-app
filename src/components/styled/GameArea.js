3e37688557e5158f3148bdf8a2c234789c0e905d
magb7 <margaux.bedu@gmail.com>
2020-06-06 18:43:15 +0200

    add props for background color and box shadow

--- src/components/styled/GameArea.js
+++ src/components/styled/GameArea.js
@@ -5,8 +5,8 @@ export default styled.div`
   margin: 30px;
   width: 600px;
   height: 450px;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: ${(props) => props.theme.color.gameArea};
   border: 3px solid #09a903;
   border-radius: 5px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: ${(props) => props.theme.color.gameShadow};
 `;
