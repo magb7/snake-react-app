ba296fc5604312329abfc0963638ab85518698a3
magb7 <margaux.bedu@gmail.com>
2020-06-06 18:03:31 +0200

    add props for text-shadow

--- src/components/styled/H3.js
+++ src/components/styled/H3.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export default styled.h3`
   color: #09a903;
-  text-shadow: 3px 2px black;
+  text-shadow: ${(props) => props.theme.color.textShadow};
+  transition: ease-in-out 0.5s all;
   line-height: 3rem;
   letter-spacing: 0.1rem;
   position: ${(props) => (props.score ? "absolute" : "")};
