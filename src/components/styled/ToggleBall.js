08d9c910be687fdc124a2cfdccfa75c787491a47
magb7 <margaux.bedu@gmail.com>
2020-06-06 17:42:31 +0200

    change props for style

--- src/components/styled/ToggleBall.js
+++ src/components/styled/ToggleBall.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default styled.div`
   width: 20px;
   height: 20px;
-  background: ${(props) => (props.dark ? "#222222" : "white")};
+  background: ${(props) => props.theme.color.background};
   border-radius: 50px;
   transform: ${(props) => (props.dark ? "translate(32px) " : "translate(0%)")};
   transition: ease-in-out 0.5s all;
