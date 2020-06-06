f00d5c59dcf7108ae18558fe0d4e59c30c8f8178
magb7 <margaux.bedu@gmail.com>
2020-06-06 18:12:06 +0200

    delete css file for header and create a styled component instead

--- src/components/styled/HeaderStyled.js
+++ src/components/styled/HeaderStyled.js
@@ -1,8 +1,10 @@
-header {
+import styled from "styled-components";
+
+export default styled.header`
   padding: 0 10vw;
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100vw;
   height: 75px;
-}
+`;
